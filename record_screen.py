584f7af696c173d83e6858f1ab860448a9509ca8
Xeth Waxman <xwaxman@gmail.com>
2020-01-11 10:41:57 -0500

    Tightened up the masking area a bit

--- record_screen.py
+++ record_screen.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
-from key_strokes import PressKey, W, A, S, D
+from key_strokes import PressKey, ReleaseKey, W, A, S, D
 
 def process_img(original_img):
     """
@@ -13,22 +13,36 @@ def process_img(original_img):
     processed_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
 
     #perform Canny edge detection
-    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    processed_img = cv2.Canny(processed_img, threshold1=300, threshold2=400)
+
+    #return the region of interest
+    vertices = np.array([[0, 600], [0, 350], [300, 150],[500, 150], [800, 350], [800, 600]])
+    processed_img = region_of_interest(processed_img, [vertices])
     return processed_img
 
+
+def region_of_interest(img, vertices):
+    """
+    Takes an image and a set of vertices of a poly and applies a mask so that
+    only the region of interest is edged
+    """
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
 
 def main():
 
     #countdown from 10 to give time to get to the GTA window
-    for i in list(range(9))[::-1]:
-        print(i+1)
-        time.sleep(1)
+    # for i in list(range(4))[::-1]:
+    #     print(i+1)
+    #     time.sleep(1)
 
 
     while(True):
         # grab the image with PIL and convert to a numpy array
         area_box = [0, 40, 800, 640]
-        PressKey(W)
+        #PressKey(W)
         img =  np.array(ImageGrab.grab(bbox=(area_box)))
         processed_img = process_img(img)
 
