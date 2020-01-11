d426a8832a78eb4847e10c016dbaca815ef7e5d1
Xeth Waxman <xwaxman@gmail.com>
2020-01-10 20:32:20 -0500

    Turned the file into a function that can be used later

--- record_screen.py
+++ record_screen.py
@@ -2,16 +2,16 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 
-# def screen_record():
-while(True):
-    # 800x600 windowed mode - add 40 vertical for the titlebar
-    area_box = [9, 40, 800, 640]
+def screen_record(area_box = [0, 40, 800, 540]):
+    """
+    Grabs an image area of the screen in a continuous loop
+    """
+    while(True):
+        # grab the image with PIL and convert to a numpy array
+        img =  np.array(ImageGrab.grab(bbox=(area_box)))
 
-    # grab the image with PIL and convert to a numpy array
-    img =  np.array(ImageGrab.grab(bbox=(area_box)))
-
-    #show and refresh the captured image, converted in RGB,  until ctrl-c is pressed
-    cv2.imshow('window',cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        #show and refresh the captured image, converted in RGB,  until ctrl-c is pressed
+        cv2.imshow('window',cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
